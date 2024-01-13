7e819ebc35e8657a69c165454448a7c6821eeb52
AdminForIinRage <bhattaraianjesh123@gmail.com>
2024-01-13 12:53:24 +1030

    Initializing vite

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from 'react-router-dom'
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout.jsx'
+import Home from './pages/Home/Home.jsx'
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout  />}>
+      <Route index element={<Home />} />
+      
+  
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "animate.css";
+
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// Define custom breakpoints
+const breakpoints = {
+  base: "0px",
+  xsm: "320px",
+  ssm: "510px", 
+  sm: "660px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  xxl: "1536px",
+  xxxl: "1950px", 
+};
+
+import "./styles/styles.css"; 
+
+// Extend Chakra UI theme with custom breakpoints and background color
+const theme = extendTheme({
+  breakpoints,
+  styles: {
+    global: {
+      html: {
+        height: "100%",
+        width: "100%",
+        // overflow: "hidden",
+      },
+      body: {
+        height: "100%",
+        width: "100%",
+       
+        
+        background: ``,
+
+        backgroundRepeat: "no-repeat",
+      },
+    },
+  },
+});
+
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById("root")
+);
